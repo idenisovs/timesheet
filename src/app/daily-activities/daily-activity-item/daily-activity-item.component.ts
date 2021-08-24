27514e2d66842bdccdf751a01a9c6f43e60dc55f
Ilya Denisov <i.denisovs@gmail.com>
2021-08-24 11:39:35 +0300

    Added basic calculations.

--- src/app/daily-activities/daily-activity-item/daily-activity-item.component.ts
+++ src/app/daily-activities/daily-activity-item/daily-activity-item.component.ts
@@ -1,7 +1,9 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { duration } from 'yet-another-duration';
+import parseDuration from 'parse-duration';
 
-const HOURS_PATTERN = /^\d\d:\d\d$/
+const HOURS_PATTERN_24 = /^([0-2]?[0-3]|[0-1]?[0-9]):[0-5][0-9]$/
 
 @Component({
   selector: 'app-daily-activity-item',
@@ -39,19 +41,19 @@ export class DailyActivityItemComponent implements OnInit {
 
   handleChanges() {
     const from = this.activity.get('from')?.value;
-    const isFromDefined = HOURS_PATTERN.test(from);
+    const isFromDefined = HOURS_PATTERN_24.test(from);
 
     const till = this.activity.get('till')?.value;
-    const isTillDefined = HOURS_PATTERN.test(till);
+    const isTillDefined = HOURS_PATTERN_24.test(till);
 
     const duration = this.activity.get('duration')?.value;
 
     if (isFromDefined && isTillDefined && !duration) {
-      return this.recalculateDuration();
+      return this.recalculateDuration(from, till);
     }
 
     if (isFromDefined && !isTillDefined && duration) {
-      return this.recalculateTillTime();
+      return this.recalculateTillTime(from, duration);
     }
 
     if (!isFromDefined && isTillDefined && duration) {
@@ -59,16 +61,56 @@ export class DailyActivityItemComponent implements OnInit {
     }
   }
 
-  recalculateDuration() {
-    console.log('Recalculate duration!');
+  recalculateDuration(from: string, till: string) {
+    const d1 = this.getDateObj(from);
+    const d2 = this.getDateObj(till);
+
+    const dT = d2.getTime() - d1.getTime();
+
+    const durationValue = duration(dT, {
+      units: {
+        min: 'minutes'
+      }
+    });
+
+    this.activity.get('duration')?.setValue(durationValue);
   }
 
-  recalculateTillTime() {
-    console.log('Recalculate till time');
+  recalculateTillTime(from: string, duration: string) {
+    const d1 = this.getDateObj(from);
+
+    const dT = parseDuration(duration);
+
+    console.log(dT);
+
+    const d2 = new Date(d1.getTime() + dT!);
+
+    console.log(d1, d2);
+
+    const hours = d2.getHours();
+
+    const minutes = d2.getMinutes();
+
+    const tillTime = (hours > 10 ? hours : '0' + hours) + ':' + (minutes > 10 ? minutes : '0' + minutes);
+
+    this.activity.get('till')?.setValue(tillTime);
+
+
   }
 
   recalculateFromTime() {
     console.log('Recalculate from time');
+  }
+
+  getDateObj(time: string): Date {
+    const [ hh, mm] = time.split(':');
+
+    const date = new Date();
+
+    date.setHours(Number(hh));
+    date.setMinutes(Number(mm));
+
+    return date;
   }
 
 }
