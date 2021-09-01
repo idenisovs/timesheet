bb3db47869281d303d7be0b851ed16b75f927318
Ilya Denisov <i.denisovs@gmail.com>
2021-09-01 18:39:52 +0300

    Added possibility to export activities as CSV file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { saveAs } from 'file-saver';
+
+import { SheetStoreService } from './services/sheet-store.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,7 +12,25 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'timesheet';
 
-  constructor() {}
+  constructor(private store: SheetStoreService) {}
 
+  async exportCsv() {
+    const sheet = await this.store.load();
 
+    const file = [ 'date;name;from;till;duration;id' ];
+
+    for (const dailySheet of sheet) {
+      for (let activity of dailySheet.activities) {
+        file.push(`${dailySheet.date};${activity.name};${activity.from};${activity.till};${activity.duration};${dailySheet.id}`);
+      }
+    }
+
+    const blob = new Blob([file.join('\n')]);
+
+    const today = new Date();
+
+    const date = today.toISOString().split('T')[0];
+
+    saveAs(blob, `timesheet-${date}.csv`);
+  }
 }
---
+++ src/app/dto/CsvRecord.ts
@@ -0,0 +1,8 @@
+export default interface CsvRecord {
+  id: number;
+  date: string;
+  name: string;
+  from: string;
+  till: string;
+  duration: string;
+}
