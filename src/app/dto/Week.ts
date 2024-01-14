89b805a6c15af6a1bc9ce2e9ebe39cbea6e4d95d
Ilja Denisovs <i.denisovs@gmail.com>
2024-01-14 11:19:59 +0200

    Mondays calculation is little simplified now.

--- src/app/dto/Week.ts
+++ src/app/dto/Week.ts
@@ -1,5 +1,11 @@
 import { Sheet } from './Sheet';
+import { getMonday } from '../utils';
 
 export class Week {
+  monday: Date;
   days: Sheet[] = [];
+
+  constructor(date: Date|string = new Date()) {
+    this.monday = getMonday(date);
+  }
 }
