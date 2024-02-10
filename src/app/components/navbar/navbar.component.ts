fd797d2f632b88da8ca9d90ae8ad783985466ca0
Ilja Denisovs <i.denisovs@gmail.com>
2024-02-10 19:26:42 +0200

    Added NavBar component.

---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgbCollapse } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    NgbCollapse
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+  isMenuCollapsed = true
+}
---
+++ src/app/pages/projects-page/projects-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './projects-page.component.html',
+  styleUrl: './projects-page.component.scss'
+})
+export class ProjectsPageComponent {
+}
