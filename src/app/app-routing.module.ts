4c606fa16781ff16e7e4cfd7f20f24706cbc9c4f
Ilja Denisovs <i.denisovs@gmail.com>
2024-02-12 20:43:52 +0200

    I decided to make the Tasks View first.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DailyActivitiesPageComponent } from './pages/daily-activities-page/daily-activities-page.component';
-import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
+import { TasksPageComponent } from './pages/tasks-page/tasks-page.component';
 
 const routes: Routes = [
   { path: '', component: DailyActivitiesPageComponent },
-  { path: 'projects', component: ProjectsPageComponent }
+  { path: 'tasks', component: TasksPageComponent }
 ];
 
 @NgModule({
