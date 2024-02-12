860247b2c10fc763c6a2cd8c14156126a006c15e
Ilja Denisovs <i.denisovs@gmail.com>
2024-02-12 21:08:47 +0200

    SheetStore got definition for Tasks table.

--- src/app/store/SheetStore.ts
+++ src/app/store/SheetStore.ts
@@ -1,16 +1,19 @@
 import Dexie from 'dexie';
-import { Sheet } from '../dto';
+import { Sheet, Task } from '../dto';
 
 export default class SheetStore extends Dexie {
   sheet: Dexie.Table<Sheet, number>;
+  tasks: Dexie.Table<Task, number>;
 
   constructor() {
     super('timesheet');
 
-    this.version(1).stores({
-      sheet: '++id,date,activities'
+    this.version(2).stores({
+      sheet: '++id,date,activities',
+      tasks: '++id,&name'
     });
 
     this.sheet = this.table('sheet');
+    this.tasks = this.table('tasks');
   }
 }
