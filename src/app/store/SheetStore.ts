453d9263dad51546b2955adc536ae37128e9f4c6
Ilja Denisovs <i.denisovs@gmail.com>
2024-02-12 22:40:58 +0200

    Added data migration for Tasks table.

--- src/app/store/SheetStore.ts
+++ src/app/store/SheetStore.ts
@@ -1,4 +1,4 @@
-import Dexie from 'dexie';
+import Dexie, { Transaction } from 'dexie';
 import { Sheet, Task } from '../dto';
 
 export default class SheetStore extends Dexie {
@@ -8,12 +8,41 @@ export default class SheetStore extends Dexie {
   constructor() {
     super('timesheet');
 
-    this.version(2).stores({
+    this.version(2.2).stores({
       sheet: '++id,date,activities',
       tasks: '++id,&name'
-    });
+    }).upgrade(this.migrateV2.bind(this));
 
     this.sheet = this.table('sheet');
     this.tasks = this.table('tasks');
+  }
+
+  async migrateV2(trans: Transaction) {
+    const sheets = await this.sheet.orderBy('date').reverse().toArray();
+    const taskNrs = this.getTaskNrs(sheets);
+
+    const tasks = Array.from(taskNrs).map((taskNr) => ({
+      name: taskNr
+    }));
+
+    return trans.table('tasks').bulkAdd(tasks)
+  }
+
+  private getTaskNrs(sheets: Sheet[]): string[] {
+    const taskNrs = new Set<string>();
+
+    sheets.forEach((timesheet) => {
+      timesheet.activities.forEach((activity) => {
+        if (!activity.name.match(/\w+-\d+/)) {
+          return;
+        }
+
+        const taskNr = activity.name.split(':')[0];
+
+        taskNrs.add(taskNr);
+      });
+    });
+
+    return Array.from(taskNrs);
   }
 }
