88f6b931464333f90ea56030fc92b7b7f77d59b3
Ilja Denisovs <i.denisovs@gmail.com>
2024-02-16 11:22:03 +0200

    Additional fields for Tasks view.

--- src/app/pages/tasks-page/tasks-page.component.ts
+++ src/app/pages/tasks-page/tasks-page.component.ts
@@ -1,19 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { SheetStoreService } from '../../services/sheet-store.service';
-import { NgForOf } from '@angular/common';
+import { DatePipe, NgForOf } from '@angular/common';
 import { Task } from '../../dto';
 
 @Component({
   selector: 'app-tasks-page',
   standalone: true,
   imports: [
-    NgForOf
+    NgForOf,
+    DatePipe
   ],
   templateUrl: './tasks-page.component.html',
   styleUrl: './tasks-page.component.scss'
 })
 export class TasksPageComponent implements OnInit {
   tasks: Task[] = [];
+  date = new Date();
 
   constructor(private sheetStore: SheetStoreService) {
   }
