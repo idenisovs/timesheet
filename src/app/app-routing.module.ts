ef73bf6ad2c9debfb84e91166ab72f8da29c9239
Ilja Denisovs <i.denisovs@gmail.com>
2024-02-25 20:56:16 +0200

    Basic Edit Issue page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DailyActivitiesPageComponent } from './pages/daily-activities-page/daily-activities-page.component';
 import { IssuesPageComponent } from './pages/issues-page/issues-page.component';
+import { IssuePageComponent } from './pages/issue-page/issue-page.component';
 
 const routes: Routes = [
   { path: '', component: DailyActivitiesPageComponent },
-  { path: 'issues', component: IssuesPageComponent }
+  { path: 'issues', component: IssuesPageComponent },
+  { path: 'issues/:issueId', component: IssuePageComponent }
 ];
 
 @NgModule({
---
+++ src/app/pages/issue-page/issue-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-issue-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './issue-page.component.html',
+  styleUrl: './issue-page.component.scss'
+})
+export class IssuePageComponent {
+
+}
