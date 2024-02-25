a806e744ffd8e2b4f0a1e4a18dcf3129c45628d6
Ilja Denisovs <i.denisovs@gmail.com>
2024-02-25 20:52:52 +0200

    Issues Pages renamed as Issues Page.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DailyActivitiesPageComponent } from './pages/daily-activities-page/daily-activities-page.component';
-import { IssuesPagesComponent } from './pages/issues-pages/issues-pages.component';
+import { IssuesPageComponent } from './pages/issues-page/issues-page.component';
 
 const routes: Routes = [
   { path: '', component: DailyActivitiesPageComponent },
-  { path: 'issues', component: IssuesPagesComponent }
+  { path: 'issues', component: IssuesPageComponent }
 ];
 
 @NgModule({
