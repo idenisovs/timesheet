45e46d8133a0784781759a82e18ef3e2d3a72074
Ilja Denisovs <i.denisovs@gmail.com>
2024-03-01 11:24:20 +0200

    TST-4: The name of activity to issue.

--- src/app/utils/issues/get-issues-from-sheets/create-issue.ts
+++ src/app/utils/issues/get-issues-from-sheets/create-issue.ts
@@ -1,9 +1,11 @@
 import { Activity, CreateIssue } from '../../../dto';
 
 export default function createIssue(issues: Map<string, CreateIssue>, issueKey: string, activity: Activity, sheetDate: Date) {
+  const [, ...issueName] = activity.name.split(':')
+
   issues.set(issueKey, {
     key: issueKey,
-    name: '',
+    name: issueName.join(':').trim(),
     activities: [activity],
     duration: activity.duration,
     createdAt: sheetDate
