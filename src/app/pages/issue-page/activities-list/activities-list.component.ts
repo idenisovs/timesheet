571fe95f1da28264f72d27e7bf44565e40fdb53e
Ilja Denisovs <i.denisovs@gmail.com>
2024-03-01 17:43:18 +0200

    TST-8: Group activities by Created At date.

--- src/app/pages/issue-page/activities-list/activities-list.component.ts
+++ src/app/pages/issue-page/activities-list/activities-list.component.ts
@@ -1,18 +1,57 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { DatePipe, NgForOf } from '@angular/common';
-import { IssueActivity } from '../../../dto';
+import { Issue, IssueActivity } from '../../../dto';
+import { getDateString } from '../../../utils';
+import { ActivitiesTableComponent } from './activities-table/activities-table.component';
 
 @Component({
   selector: 'app-activities-list',
   standalone: true,
   imports: [
     NgForOf,
-    DatePipe
+    DatePipe,
+    ActivitiesTableComponent
   ],
   templateUrl: './activities-list.component.html',
   styleUrl: './activities-list.component.scss'
 })
-export class ActivitiesListComponent {
+export class ActivitiesListComponent implements OnInit {
   @Input()
   activities: IssueActivity[] = [];
+
+  activitiesByDate = new Map<string, IssueActivity[]>();
+  activityDates: string[] = [];
+
+  ngOnInit() {
+    this.groupByDate();
+    this.updateActivityDates();
+  }
+
+  private groupByDate() {
+    this.activities.forEach((activity: IssueActivity) => {
+      const activityDate = getDateString(activity.createdAt);
+
+      if (this.activitiesByDate.has(activityDate)) {
+        this.activitiesByDate.get(activityDate)?.push(activity);
+      } else {
+        this.activitiesByDate.set(activityDate, [ activity ]);
+      }
+    });
+  }
+
+  private updateActivityDates() {
+    const dates = Array.from(this.activitiesByDate.keys());
+
+    dates.sort((a: string, b: string) => {
+      if (a > b) {
+        return -1;
+      } else if (a < b) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    this.activityDates = dates;
+  }
 }
