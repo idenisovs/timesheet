a463b5d07007641d27e56a4cf92c5e5715bbeec5
Ilja Denisovs <i.denisovs@gmail.com>
2024-03-12 15:39:22 +0200

    TST-13: Added basic Create Project Modal.

--- src/app/pages/projects-page/projects-page.component.ts
+++ src/app/pages/projects-page/projects-page.component.ts
@@ -1,7 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NgForOf } from '@angular/common';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Subscription } from 'rxjs';
 import { Project } from '../../dto';
 import { ProjectCardComponent } from './project-card/project-card.component';
-import { NgForOf } from '@angular/common';
+import { ActionsService } from '../../services/actions.service';
+import { Actions } from '../../services/Actions';
+import { CreateProjectModalComponent } from './create-project-modal/create-project-modal.component';
+import { handleModalResult } from '../../utils';
 
 @Component({
   selector: 'app-projects-page',
@@ -13,7 +19,7 @@ import { NgForOf } from '@angular/common';
   templateUrl: './projects-page.component.html',
   styleUrl: './projects-page.component.scss'
 })
-export class ProjectsPageComponent {
+export class ProjectsPageComponent implements OnInit, OnDestroy {
   projects: Project[] = [{
     id: crypto.randomUUID().toString(),
     name: 'Timesheet',
@@ -26,4 +32,33 @@ export class ProjectsPageComponent {
     keys: 'MS',
     createdAt: new Date()
   }];
+
+  actionsSubscription?: Subscription;
+
+  constructor(
+    private modal: NgbModal,
+    private actions: ActionsService
+  ) {}
+
+  ngOnInit() {
+    this.actionsSubscription = this.actions.on.subscribe(this.actionsHandler.bind(this));
+  }
+
+  ngOnDestroy() {
+    this.actionsSubscription?.unsubscribe();
+  }
+
+  actionsHandler(action: Actions) {
+    if (action === Actions.AddProject) {
+      void this.showAddProjectModal();
+    }
+  }
+
+  async showAddProjectModal() {
+    const createProjectModal = this.modal.open(CreateProjectModalComponent);
+
+    const createdProject = await handleModalResult<Project|null>(createProjectModal.result);
+
+    console.log(createdProject);
+  }
 }
