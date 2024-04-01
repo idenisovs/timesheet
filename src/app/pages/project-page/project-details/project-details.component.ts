75e2f47c087d8c0311ab511537cb744af84aaec6
Ilja Denisovs <i.denisovs@gmail.com>
2024-04-01 15:06:14 +0300

    Minor bugs in Project view.

--- src/app/pages/project-page/project-details/project-details.component.ts
+++ src/app/pages/project-page/project-details/project-details.component.ts
@@ -1,8 +1,9 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { DatePipe, NgForOf, NgIf, PercentPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
 import { Issue, Project } from '../../../dto';
 import { IssuesService } from '../../../services/issues.service';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-project-details',
