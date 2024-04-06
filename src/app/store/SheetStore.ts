b56a77ebe56072439aa79f672ea8b61e35b267f7
Ilja Denisovs <i.denisovs@gmail.com>
2024-04-06 14:42:14 +0300

    TST-17: Data migration for weeks and activities.

--- src/app/store/SheetStore.ts
+++ src/app/store/SheetStore.ts
@@ -1,12 +1,16 @@
 import Dexie, { Transaction } from 'dexie';
-import { Sheet, Issue, Project } from '../dto';
+import { Sheet, Issue, Project, Week, Activity } from '../dto';
 import migrateV2 from './migrate-v2';
 import migrateV3 from './migrate-v3';
+import migrateV5 from './migrate-v5';
 
 export default class SheetStore extends Dexie {
   sheet: Dexie.Table<Sheet, number>;
   issues: Dexie.Table<Issue, number>;
   projects: Dexie.Table<Project, string>;
+
+  weeks: Dexie.Table<Week, string>;
+  activities: Dexie.Table<Activity, string>;
 
   constructor() {
     super('timesheet');
@@ -27,8 +31,16 @@ export default class SheetStore extends Dexie {
       projects: 'id,&name,description,*keys,createdAt'
     });
 
+    this.version(14).stores({
+      weeks: 'id,start,end',
+      activities: 'id,name,date,weekId'
+    }).upgrade((tx: Transaction) => migrateV5(this, tx));
+
     this.sheet = this.table('sheet');
     this.issues = this.table('issues');
     this.projects = this.table('projects');
+
+    this.weeks = this.table('weeks');
+    this.activities = this.table('activities');
   }
 }
