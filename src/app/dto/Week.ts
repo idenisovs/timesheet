4b8d54fd6d597090ef55106da2c22e4d1073cd20
Ilja Denisovs <i.denisovs@gmail.com>
2024-04-06 19:52:09 +0300

    TST-17: Week, Day and Activity DTOs.

--- src/app/dto/Week.ts
+++ src/app/dto/Week.ts
@@ -1,12 +1,36 @@
 import { endOfDay, getMonday, getSunday, startOfDay } from '../utils';
+import { Day } from './Day';
+import { WeekEntity } from '../store/entities/week-entity';
 
 export class Week {
   id: string = crypto.randomUUID();
-  from = new Date();
-  till = new Date();
+  from: Date;
+  till: Date;
+  days: Day[] = [];
 
   constructor(date = new Date()) {
     this.from = startOfDay(getMonday(date));
     this.till = endOfDay(getSunday(date));
+  }
+
+  static build(source: WeekEntity): Week {
+    const week = new Week();
+
+    week.id = source.id;
+    week.from = new Date(source.from);
+    week.till = new Date(source.till);
+    week.days = [];
+
+    return week;
+  }
+
+  static entity(source: Week): WeekEntity {
+    const { id, from, till } = source;
+
+    return {
+      id,
+      from: from.toISOString(),
+      till: till.toISOString()
+    };
   }
 }
