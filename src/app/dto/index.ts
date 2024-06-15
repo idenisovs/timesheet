9358e2b75c642d1647177277151f28ded03ca07e
Ilja Denisovs <i.denisovs@gmail.com>
2024-06-15 14:34:04 +0300

    TST-17: Displaying Day items.

--- src/app/dto/index.ts
+++ src/app/dto/index.ts
@@ -2,5 +2,6 @@ export * from './Activity';
 export * from './CsvRecord';
 export * from './Sheet';
 export * from './Week';
+export * from './Day';
 export * from './Issue';
 export * from './Project';
