e56de930ed33586e21dc574b16f95d81fd35f323
Ilja Denisovs <i.denisovs@gmail.com>
2024-06-15 18:58:33 +0300

    TST-17: Added Form definition for Daily Activities.

--- src/app/components/daily-activities-week-day/daily-activities-week-day.component.ts
+++ src/app/components/daily-activities-week-day/daily-activities-week-day.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Day } from '../../dto';
 import { DatePipe, JsonPipe } from '@angular/common';
+import { Form, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-daily-activities-week-day',
@@ -15,4 +16,17 @@ import { DatePipe, JsonPipe } from '@angular/common';
 export class DailyActivitiesWeekDayComponent {
   @Input()
   day!: Day;
+
+  form = this.fb.group({
+    activities: this.fb.array([
+      this.fb.group({
+        name: [''],
+        from: [''],
+        till: [''],
+        duration: ['']
+      })
+    ])
+  });
+
+  constructor(private fb: FormBuilder) {}
 }
