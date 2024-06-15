064fbceec09eff86f1e18b24da1bc4bacde0b6b0
Ilja Denisovs <i.denisovs@gmail.com>
2024-06-15 19:22:15 +0300

    TST-17: prepareForToday is moved to Daily Activities Page.

--- src/app/pages/daily-activities-page/daily-activities-page.component.ts
+++ src/app/pages/daily-activities-page/daily-activities-page.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { SheetStoreService } from '../../services/sheet-store.service';
-import { Week } from '../../dto';
+import { Day, Week } from '../../dto';
 import { WeeksRepositoryService } from '../../repository/weeks-repository.service';
 
 @Component({
@@ -18,8 +18,32 @@ export class DailyActivitiesPageComponent implements OnInit {
   ) { }
 
   async ngOnInit() {
-    await this.store.prepareForToday();
+    await this.prepareForToday();
 
     this.weeks = await this.weeksRepo.getAll();
+  }
+
+  private async prepareForToday(): Promise<void> {
+    const today = new Date();
+
+    today.setHours(0, 0, 0, 0);
+
+    const db = this.store.Instance;
+
+    let currentWeek = await db.weeks.where('till').aboveOrEqual(today).first();
+
+    if (!currentWeek) {
+      const week = new Week(today);
+      currentWeek = Week.entity(week);
+      await db.weeks.add(currentWeek);
+    }
+
+    const currentDay = await db.days.where('date').equals(today.toISOString()).first();
+
+    if (!currentDay) {
+      const day = new Day();
+      day.weekId = currentWeek.id;
+      await db.days.add(Day.entity(day));
+    }
   }
 }
