1d4c87a79a721e6545a607f78e0955f765588599
Ilja Denisovs <i.denisovs@gmail.com>
2024-06-29 20:28:02 +0300

    TST-17: Activity Form Item should be added as FormGroup instance.

--- src/app/components/daily-activities-week-day/daily-activities-week-day.service.ts
+++ src/app/components/daily-activities-week-day/daily-activities-week-day.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
 
 import { Activity, Day } from '../../dto';
+import { ActivityFormItem } from './ActivityFormItem';
+import { FormBuilder } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DailyActivitiesWeekDayService {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
+
+  makeActivityFormItem(activity?: Activity) {
+    return this.fb.group(new ActivityFormItem(activity));
+  }
 
   createActivity(formValue: any, day?: Day) {
     const activity = new Activity();
