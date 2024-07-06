cebd24b1450b2b4d4012229cef03cc7a26a5f92d
Ilja Denisovs <i.denisovs@gmail.com>
2024-07-06 23:30:05 +0300

    TST-17: Housekeeping on Issue Service.

--- src/app/pages/project-page/project-page.service.ts
+++ src/app/pages/project-page/project-page.service.ts
@@ -1,24 +1,29 @@
 import { Injectable } from '@angular/core';
+
 import { Issue, Project } from '../../dto';
 import { IssuesService } from '../../services/issues.service';
+import { IssueRepositoryService } from '../../repository/issue-repository.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectPageService {
 
-  constructor(private issues: IssuesService) {}
+  constructor(
+    private issueRepository: IssueRepositoryService,
+    private issueService: IssuesService,
+  ) {}
 
   async getProjectIssues(project: Project): Promise<Issue[]> {
     const result: Issue[] = []
 
     for (let key of project.keys) {
-      const issues = await this.issues.getByKey(key);
+      const issues = await this.issueRepository.getByKeyPrefix(key);
 
       result.push(...issues);
     }
 
-    result.sort(this.issues.sort);
+    result.sort(this.issueService.sort);
 
     return result;
   }
