3d93c2ec43de1c6a1a68b30b17a54254a7d01422
Ilja Denisovs <i.denisovs@gmail.com>
2024-07-26 21:04:55 +0300

    TST-17: Project Repository got getById method.

--- src/app/repository/project-repository.service.ts
+++ src/app/repository/project-repository.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
+
 import { SheetStoreService } from '../services/sheet-store.service';
+import { Project } from '../dto';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +13,15 @@ export class ProjectRepositoryService {
 
   async getAll() {
     return this.db.projects.toArray();
+  }
+
+  async getById(projectId: string): Promise<Project|null> {
+    const record = await this.db.projects.where('id').equals(projectId).first();
+
+    if (record) {
+      return Project.fromRecord(record);
+    }
+
+    return null;
   }
 }
