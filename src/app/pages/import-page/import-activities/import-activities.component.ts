22855c15022bf1ef5f78df3ba7dbd5638ad6bf65
Ilja Denisovs <i.denisovs@gmail.com>
2024-07-28 19:41:04 +0300

    TST-18: Saving all new activities action.

--- src/app/pages/import-page/import-activities/import-activities.component.ts
+++ src/app/pages/import-page/import-activities/import-activities.component.ts
@@ -4,6 +4,7 @@ import { NgForOf } from '@angular/common';
 import { Activity } from '../../../dto';
 import { ImportedIssueDiffComponent } from '../import-issues/imported-issue-diff/imported-issue-diff.component';
 import { ImportedActivityDiffComponent } from './imported-activity-diff/imported-activity-diff.component';
+import { ActivitiesRepositoryService } from '../../../repository/activities-repository.service';
 
 @Component({
   selector: 'app-import-activities',
@@ -20,8 +21,36 @@ export class ImportActivitiesComponent {
   @Input()
   importedActivities!: Activity[];
 
+  constructor(
+    private activityRepository: ActivitiesRepositoryService
+  ) {}
+
   removeCompletedActivity(activity: Activity) {
     const idx = this.importedActivities.indexOf(activity);
     this.importedActivities.splice(idx, 1);
+  }
+
+  saveAll() {}
+
+  async saveNewActivities() {
+    let savedActivitiesCount = 0;
+
+    for (let activity of this.importedActivities) {
+      const existingActivity = await this.activityRepository.getById(activity.id);
+
+      if (existingActivity) {
+        continue;
+      }
+
+      await this.activityRepository.save([activity]);
+      this.removeCompletedActivity(activity);
+      savedActivitiesCount++;
+    }
+
+    alert(`Saved ${savedActivitiesCount} activities!`);
+  }
+
+  cancelImportingActivities() {
+    this.importedActivities.splice(0);
   }
 }
