05eb106e1e06a3582d4298909efda5698866a420
Ilja Denisovs <i.denisovs@gmail.com>
2024-07-29 22:14:21 +0300

    TST-18: Exporting ISO string for Dates.

--- src/app/workflows/export-workflow.service.ts
+++ src/app/workflows/export-workflow.service.ts
@@ -3,7 +3,7 @@ import * as XLSX from 'xlsx';
 
 import { getDateString } from '../utils';
 import { ActivitiesRepositoryService } from '../repository/activities-repository.service';
-import { Activity, Day, Issue, Project } from '../dto';
+import { Activity, Issue, Project } from '../dto';
 import { DaysRepositoryService } from '../repository/days-repository.service';
 import { IssueRepositoryService } from '../repository/issue-repository.service';
 import { ProjectRepositoryService } from '../repository/project-repository.service';
@@ -37,13 +37,12 @@ export class ExportWorkflowService {
   }
 
   async getActivitiesSheet() {
-    const days = await this.daysRepository.getAll();
     const activities = await this.activitiesRepository.getAll() as Activity[];
 
     const activitiesJson = activities.map((activity: Activity) => {
       return {
         ...activity,
-        date: this.getDateOfActivity(days, activity)
+        date: activity.date.toISOString()
       };
     })
 
@@ -57,7 +56,7 @@ export class ExportWorkflowService {
       delete issue.activities;
       return {
         ...issue,
-        createdAt: this.getDateStr(issue.createdAt as Date)
+        createdAt: issue.createdAt?.toISOString()
       }
     });
 
@@ -69,38 +68,10 @@ export class ExportWorkflowService {
 
     const projectsJson = projects.map((project: Project) => ({
       ...project,
-      createdAt: this.getDateStr(project.createdAt),
+      createdAt: project.createdAt.toISOString(),
       keys: project.keys.join(';')
     }));
 
     return XLSX.utils.json_to_sheet(projectsJson);
-  }
-
-  getDateOfActivity(days: Day[], activity: Activity) {
-    const day = days.find((day) => activity.dayId === day.id);
-
-    if (day) {
-      return this.getDateStr(day.date);
-    } else {
-      return this.getDateStr(activity.date);
-    }
-  }
-
-  getDateStr(date: Date) {
-    const day = date.getDate();
-    let dayStr = day.toString();
-    if (day < 10) {
-      dayStr = '0' + dayStr;
-    }
-
-    const month = date.getMonth() + 1;
-    let monthStr = month.toString();
-    if (month < 10) {
-      monthStr = '0' + month;
-    }
-
-    const yearStr = date.getFullYear().toString();
-
-    return `${yearStr}-${monthStr}-${dayStr}`;
   }
 }
