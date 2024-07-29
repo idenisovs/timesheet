5a8d06e040b7d96645c503ed6ce29093b0675962
Ilja Denisovs <i.denisovs@gmail.com>
2024-07-29 22:16:31 +0300

    TST-18: Removed redundant Import Modal component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DailyActivitiesPageComponent } from './pages/daily-activities-page/daily-activities-page.component';
-import { ImportModalComponent } from './components/import-modal/import-modal.component';
 import { DailyActivitiesWeekComponent } from './components/daily-activities-week/daily-activities-week.component';
 import { DailyActivitiesWeekHeaderComponent } from './components/daily-activities-week/daily-activities-week-header/daily-activities-week-header.component';
 import { MissingDailyActivityComponent } from './components/missing-daily-activity/missing-daily-activity.component';
@@ -23,7 +22,6 @@ import {
   declarations: [
     AppComponent,
     DailyActivitiesPageComponent,
-    ImportModalComponent,
     DailyActivitiesWeekComponent,
     DailyActivitiesWeekHeaderComponent,
     MissingDailyActivityComponent
