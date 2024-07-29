269e4912b7db0dcd69c39e15f9fb30704a85a79f
Ilja Denisovs <i.denisovs@gmail.com>
2024-07-29 11:25:11 +0300

    TST-18: Imported Issues component got Import All, Import New and Cancel actions.

--- src/app/pages/import-page/import-issues/import-issues.component.ts
+++ src/app/pages/import-page/import-issues/import-issues.component.ts
@@ -4,6 +4,7 @@ import { NgForOf } from '@angular/common';
 import { Issue } from '../../../dto';
 import { ImportedProjectDiffComponent } from '../import-projects/imported-project-diff/imported-project-diff.component';
 import { ImportedIssueDiffComponent } from './imported-issue-diff/imported-issue-diff.component';
+import {IssueRepositoryService} from "../../../repository/issue-repository.service";
 
 @Component({
   selector: 'app-import-issues',
@@ -20,8 +21,56 @@ export class ImportIssuesComponent {
   @Input()
   importedIssues: Issue[] = [];
 
+  constructor(private issueRepo: IssueRepositoryService) {
+  }
+
   removeCompletedIssue(issue: Issue) {
     const idx = this.importedIssues.indexOf(issue);
     this.importedIssues.splice(idx, 1);
+  }
+
+  async saveAll() {
+    let importedIssuesCount = 0;
+
+    for (let idx = 0; idx < this.importedIssues.length; idx++) {
+      const importedIssue = this.importedIssues[idx];
+
+      const existingIssue = await this.issueRepo.getById(importedIssue.id);
+
+      if (existingIssue) {
+        await this.issueRepo.update(importedIssue);
+      } else {
+        await this.issueRepo.create(importedIssue);
+      }
+
+      this.importedIssues.splice(idx, 1);
+      idx--;
+      importedIssuesCount++;
+    }
+  }
+
+  async saveNew() {
+    let importedIssuesCount = 0;
+
+    for (let idx = 0; idx < this.importedIssues.length; idx++) {
+      const importedIssue = this.importedIssues[idx];
+
+      const existingIssue = await this.issueRepo.getById(importedIssue.id);
+
+      if (existingIssue) {
+        continue;
+      }
+
+      await this.issueRepo.create(importedIssue);
+      this.importedIssues.splice(idx, 1);
+      idx--;
+      importedIssuesCount++;
+    }
+
+    alert(`Imported ${importedIssuesCount} issues!`);
+  }
+
+  cancel() {
+    this.importedIssues.splice(0);
   }
 }
