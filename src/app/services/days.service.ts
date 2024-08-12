8843fbe590f5fc2570657e9783efeca61b445130
Ilja Denisovs <i.denisovs@gmail.com>
2024-08-12 22:46:21 +0300

    TST-23: Removed Days Summary DTO.

--- src/app/services/days.service.ts
+++ src/app/services/days.service.ts
@@ -1,39 +1,13 @@
 import { Injectable } from '@angular/core';
 
-import { Day, Week, DaysSummary } from '../dto';
-import { calculateTotalDuration, getDateString, startOfDay } from '../utils';
-import { ActivitiesRepositoryService } from '../repository/activities-repository.service';
+import { Day, Week } from '../dto';
+import { startOfDay } from '../utils';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DaysService {
-  constructor(
-    private activityRepository: ActivitiesRepositoryService
-  ) { }
-
-  findByDate(days: Day[], date: Date): Day | undefined {
-    const targetDate = getDateString(date);
-
-    return days.find((day: Day) => {
-      return targetDate === getDateString(day.date);
-    });
-  }
-
-  async getSummary(days: Day[]): Promise<DaysSummary> {
-    const result: DaysSummary = {
-      activities: 0,
-      duration: 0
-    };
-
-    for (let day of days) {
-      const activities = await this.activityRepository.getByDay(day);
-      result.activities += activities.length;
-      result.duration += calculateTotalDuration(activities);
-    }
-
-    return result;
-  }
+  constructor() { }
 
   addMissingDays(week: Week, days: Day[]) {
     let expectedDate = startOfDay(week.till);
