34d7f5d57c9711ca25f04db743fac1f06cc7c913
Ilja Denisovs <i.denisovs@gmail.com>
2024-08-15 12:55:41 +0300

    TST-25: Retrieving basic data for Weekly Overview.

--- src/app/components/daily-activities-week/weekly-overview-modal/weekly-overview-modal.component.ts
+++ src/app/components/daily-activities-week/weekly-overview-modal/weekly-overview-modal.component.ts
@@ -1,7 +1,9 @@
-import {Component, Input} from '@angular/core';
-import {DatePipe, JsonPipe, NgForOf, NgIf} from "@angular/common";
-import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
-import {Week} from "../../../dto";
+import { Component, Input, OnInit } from '@angular/core';
+import { DatePipe, JsonPipe, NgForOf, NgIf } from '@angular/common';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+import { Week } from '../../../dto';
+import { WeeklyOverviewModalService } from './weekly-overview-modal.service';
 
 @Component({
   selector: 'app-weekly-overview-modal',
@@ -15,9 +17,20 @@ import {Week} from "../../../dto";
   templateUrl: './weekly-overview-modal.component.html',
   styleUrl: './weekly-overview-modal.component.scss'
 })
-export class WeeklyOverviewModalComponent {
+export class WeeklyOverviewModalComponent implements OnInit {
   @Input()
   week!: Week;
 
-  constructor(public modal: NgbActiveModal) {}
+  constructor(
+    public modal: NgbActiveModal,
+    private weeklyOverviewService: WeeklyOverviewModalService
+  ) {}
+
+  async ngOnInit() {
+    if (!this.week) {
+      return;
+    }
+
+    await this.weeklyOverviewService.run(this.week);
+  }
 }
