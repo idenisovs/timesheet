2d8b9821ae5c779a58b48f84b408ddd4ed72f1ad
Ilja Denisovs <i.denisovs@gmail.com>
2024-09-04 22:08:30 +0300

    TST-35: Basic toggler for color mode.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActionsService } from './services/actions.service';
+import { Subscription } from 'rxjs';
+import { Actions } from './services/Actions';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'timesheet';
+  actionsSubs!: Subscription;
 
   get IsUsingHttp(): boolean {
     return window.location.protocol === 'http:';
   }
 
-  constructor() {}
+  constructor(private actionsService: ActionsService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.actionsSubs = this.actionsService.on.subscribe(this.handlePageActions.bind(this));
+  }
+
+  ngOnDestroy() {
+    this.actionsSubs.unsubscribe();
+  }
+
+  private async handlePageActions(action: Actions) {
+    switch (action) {
+      case Actions.ToggleMode:
+        this.toggleMode();
+        break;
+    }
+  }
+
+  private toggleMode() {
+    document.documentElement.setAttribute('data-bs-theme', 'light');
+  }
 }
