016c0b2b5983cdbebec1a4476ad2074defdd2370
Ilja Denisovs <i.denisovs@gmail.com>
2024-09-13 20:08:46 +0300

    TST-36: Handling filter changes.

--- src/app/pages/analytics-page/analytics-page.component.ts
+++ src/app/pages/analytics-page/analytics-page.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NgbInputDatepicker } from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-analytics-page',
   standalone: true,
   imports: [
     NgbInputDatepicker,
+    ReactiveFormsModule,
   ],
   templateUrl: './analytics-page.component.html',
   styleUrl: './analytics-page.component.scss'
 })
-export class AnalyticsPageComponent {
+export class AnalyticsPageComponent implements OnInit {
+  filtersForm = this.fb.group({
+    dateFrom: [{ year: 2024, month: 9, day: 1}],
+    dateTill: [{ year: 2024, month: 9, day: 13}]
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.filtersForm.valueChanges.subscribe((changes) => {
+      console.log(changes);
+    });
+  }
 }
