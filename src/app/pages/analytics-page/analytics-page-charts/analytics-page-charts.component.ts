826b18a3df1bc4134ec5b4d988e196ee1b750437
Ilja Denisovs <i.denisovs@gmail.com>
2024-11-09 17:23:37 +0200

    TST-55: Displaying Project Ratio Pie Chart

--- src/app/pages/analytics-page/analytics-page-charts/analytics-page-charts.component.ts
+++ src/app/pages/analytics-page/analytics-page-charts/analytics-page-charts.component.ts
@@ -1,14 +1,23 @@
 import { Component, Input } from '@angular/core';
 import { Analytics } from '../types';
+import { BaseChartDirective } from 'ng2-charts';
+import { NgIf } from '@angular/common';
+import { ProjectRatioChartComponent } from './project-ratio-chart/project-ratio-chart.component';
 
 @Component({
   selector: 'app-analytics-page-charts',
   standalone: true,
-  imports: [],
+  imports: [
+    BaseChartDirective,
+    NgIf,
+    ProjectRatioChartComponent,
+  ],
   templateUrl: './analytics-page-charts.component.html',
   styleUrl: './analytics-page-charts.component.scss'
 })
 export class AnalyticsPageChartsComponent {
+  constructor() {}
+
   @Input()
   analytics!: Analytics;
 }
---
+++ src/app/pages/analytics-page/analytics-page-charts/project-ratio-chart/project-ratio-chart.component.ts
@@ -0,0 +1,66 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { BaseChartDirective } from 'ng2-charts';
+import { NgIf, PercentPipe } from '@angular/common';
+import { ChartData, ChartOptions, TooltipItem } from 'chart.js';
+
+import { Analytics } from '../../types';
+
+@Component({
+  selector: 'app-project-ratio-chart',
+  standalone: true,
+	imports: [
+		BaseChartDirective,
+		NgIf
+	],
+  providers: [PercentPipe],
+  templateUrl: './project-ratio-chart.component.html',
+  styleUrl: './project-ratio-chart.component.scss'
+})
+export class ProjectRatioChartComponent implements OnInit {
+  load = false;
+
+  data: ChartData <'pie', number[]> = {
+    labels: [],
+    datasets: [{
+      data:[],
+    }]
+  };
+
+  options: ChartOptions<'pie'> = {
+    plugins: {
+      title: {
+        display: true,
+        text: 'Projects Time Ratio'
+      },
+      tooltip: {
+        callbacks: {
+          label: this.formatLabel.bind(this)
+        }
+      }
+    }
+  };
+
+  @Input()
+  analytics!: Analytics;
+
+  constructor(private percentPipe: PercentPipe) {}
+
+  ngOnInit() {
+    this.updateProjectDataset();
+
+    setTimeout(() => {
+      this.load = true;
+    }, 200);
+  }
+
+  updateProjectDataset() {
+    for (const po of this.analytics.projectOverview) {
+      this.data.labels?.push(po.project.name);
+      this.data.datasets[0].data.push(po.durationRatio);
+    }
+  }
+
+  formatLabel(item: TooltipItem<'pie'>): string {
+    return this.percentPipe.transform(item.raw as number) as string;
+  }
+}
