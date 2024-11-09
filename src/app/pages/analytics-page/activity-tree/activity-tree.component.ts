f205c189a7e95d7e100c53ba552327c011d4e35c
Ilja Denisovs <i.denisovs@gmail.com>
2024-11-09 13:48:52 +0200

    TST-54: Add some totals to Analytics table.

--- src/app/pages/analytics-page/activity-tree/activity-tree.component.ts
+++ src/app/pages/analytics-page/activity-tree/activity-tree.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { KeyValuePipe, NgForOf, PercentPipe } from '@angular/common';
 
 import { ProjectOverview } from '../../../dto';
 import { IssueRowComponent } from './issue-row/issue-row.component';
 import { ActivityRowComponent } from './activity-row/activity-row.component';
 import { ProjectRowComponent } from './project-row/project-row.component';
+import { Totals } from '../types';
+import { DurationService } from '../../../services/duration.service';
 
 @Component({
   selector: 'app-activity-tree',
@@ -20,7 +22,7 @@ import { ProjectRowComponent } from './project-row/project-row.component';
   templateUrl: './activity-tree.component.html',
   styleUrl: './activity-tree.component.scss'
 })
-export class ActivityTreeComponent {
+export class ActivityTreeComponent implements OnInit {
   @Input()
   activityTree!: ProjectOverview[];
 
@@ -29,4 +31,27 @@ export class ActivityTreeComponent {
 
   @Input()
   isIssuesVisible!: boolean;
+
+  totals: Totals = {
+    activities: 0,
+    time: '0',
+    rate: 0
+  }
+
+  constructor(
+    private durationService: DurationService
+  ) {}
+
+  ngOnInit() {
+    this.calculateTotals();
+  }
+
+  calculateTotals() {
+    this.totals = this.activityTree.reduce((result: Totals, projectOverview: ProjectOverview) => {
+      result.activities += projectOverview.activities.length;
+      result.time = this.durationService.sum([result.time, projectOverview.duration]);
+      result.rate += projectOverview.durationRatio;
+      return result;
+    }, { activities: 0, time: '0', rate: 0 });
+  }
 }
--- src/app/pages/analytics-page/types.ts
+++ src/app/pages/analytics-page/types.ts
@@ -1,3 +1,9 @@
 import { Activity, Issue, Project } from '../../dto';
 
 export type ActivityTree = Map<Project, Map<Issue, Activity[]>>;
+
+export type Totals = {
+  activities: number,
+  time: string,
+  rate: number
+};
