9760e45a029c90180868634cf681a34ec19faebe
Ilja Denisovs <i.denisovs@gmail.com>
2024-11-09 15:34:16 +0200

    TST-55: Add Charts tab component.

--- src/app/pages/analytics-page/analytics-page-tabs/analytics-page-tabs.component.ts
+++ src/app/pages/analytics-page/analytics-page-tabs/analytics-page-tabs.component.ts
@@ -3,6 +3,7 @@ import { Component, Input } from '@angular/core';
 import { ActivityTreeComponent } from '../activity-tree/activity-tree.component';
 import { NgbNav, NgbNavContent, NgbNavItem, NgbNavLinkButton, NgbNavOutlet } from '@ng-bootstrap/ng-bootstrap';
 import { Analytics } from '../types';
+import { AnalyticsPageChartsComponent } from '../analytics-page-charts/analytics-page-charts.component';
 
 @Component({
   selector: 'app-analytics-page-tabs',
@@ -14,6 +15,7 @@ import { Analytics } from '../types';
     NgbNavLinkButton,
     NgbNavItem,
     NgbNavOutlet,
+    AnalyticsPageChartsComponent,
   ],
   templateUrl: './analytics-page-tabs.component.html',
   styleUrl: './analytics-page-tabs.component.scss',
