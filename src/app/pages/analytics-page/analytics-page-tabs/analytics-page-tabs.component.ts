0a6adf39f584040ef869913e8230f1244f166a26
Ilja Denisovs <i.denisovs@gmail.com>
2024-11-09 14:37:46 +0200

    TST-54: Activity Page Tabs are moved to separate component.

---
+++ src/app/pages/analytics-page/analytics-page-tabs/analytics-page-tabs.component.ts
@@ -0,0 +1,32 @@
+import { Component, Input } from '@angular/core';
+
+import { ActivityTreeComponent } from '../activity-tree/activity-tree.component';
+import { NgbNav, NgbNavContent, NgbNavItem, NgbNavLinkButton, NgbNavOutlet } from '@ng-bootstrap/ng-bootstrap';
+import { ProjectOverview } from '../../../dto';
+
+@Component({
+  selector: 'app-analytics-page-tabs',
+  standalone: true,
+  imports: [
+    ActivityTreeComponent,
+    NgbNav,
+    NgbNavContent,
+    NgbNavLinkButton,
+    NgbNavItem,
+    NgbNavOutlet,
+  ],
+  templateUrl: './analytics-page-tabs.component.html',
+  styleUrl: './analytics-page-tabs.component.scss',
+})
+export class AnalyticsPageTabsComponent {
+  active = 1;
+
+  @Input()
+  isActivitiesVisible = false;
+
+  @Input()
+  isIssuesVisible = false;
+
+  @Input()
+  activityTree: ProjectOverview[] = [];
+}
