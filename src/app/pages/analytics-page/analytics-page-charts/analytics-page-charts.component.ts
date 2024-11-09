b3c30ce73ee99d2cd18f1ff259846e8efdd9a865
Ilja Denisovs <i.denisovs@gmail.com>
2024-11-09 20:39:02 +0200

    TST-56: Added basic Weeks Worked Time chart component.

--- src/app/pages/analytics-page/analytics-page-charts/analytics-page-charts.component.ts
+++ src/app/pages/analytics-page/analytics-page-charts/analytics-page-charts.component.ts
@@ -3,21 +3,23 @@ import { Analytics } from '../types';
 import { BaseChartDirective } from 'ng2-charts';
 import { NgIf } from '@angular/common';
 import { ProjectRatioChartComponent } from './project-ratio-chart/project-ratio-chart.component';
+import { WeeksWorkedTimeComponent } from './weeks-worked-time/weeks-worked-time.component';
 
 @Component({
   selector: 'app-analytics-page-charts',
   standalone: true,
-  imports: [
-    BaseChartDirective,
-    NgIf,
-    ProjectRatioChartComponent,
-  ],
+	imports: [
+		BaseChartDirective,
+		NgIf,
+		ProjectRatioChartComponent,
+		WeeksWorkedTimeComponent,
+	],
   templateUrl: './analytics-page-charts.component.html',
   styleUrl: './analytics-page-charts.component.scss'
 })
 export class AnalyticsPageChartsComponent {
-  constructor() {}
+	@Input()
+	analytics!: Analytics;
 
-  @Input()
-  analytics!: Analytics;
+	constructor() {}
 }
