ff5b191f1cad10d323a8cc8179fe50d7606afb2a
Ilja Denisovs <i.denisovs@gmail.com>
2024-11-09 20:39:27 +0200

    TST-56: Added calculateWeeklyHours method to Analytics service.

--- src/app/pages/analytics-page/analytics-page.service.ts
+++ src/app/pages/analytics-page/analytics-page.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
+import { DateTime } from 'luxon';
+import parseDuration from 'parse-duration';
 
 import { ActivitiesRepositoryService } from '../../repository/activities-repository.service';
-import { Day, ProjectOverview } from '../../dto';
+import { Activity, Day, ProjectOverview } from '../../dto';
 import { AnalyticsPageFilters } from './AnalyticsPageFilterForm';
 import { DaysRepositoryService } from '../../repository/days-repository.service';
 import { OverviewService } from '../../services/overview.service';
@@ -24,6 +26,9 @@ export class AnalyticsPageService {
     const activities = await this.activityRepository.getByDays(days);
     const projectOverview = await this.overviewService.getProjectOverview(activities)
     const totals = this.calculateTotals(projectOverview);
+    const weeklyHours = this.calculateWeeklyHours(activities);
+
+    console.log(weeklyHours);
 
     return {
       projectOverview, totals
@@ -49,5 +54,18 @@ export class AnalyticsPageService {
       result.rate += projectOverview.durationRatio;
       return result;
     }, { activities: 0, time: '0', rate: 0 });
+  }
+
+  calculateWeeklyHours(activities: Activity[]) {
+    const weeklyHours = new Map<number, number>();
+
+    for (const activity of activities) {
+      const weekNumber = DateTime.fromJSDate(activity.date).weekNumber;
+      const activityTimeMs = parseDuration(activity.duration) ?? 0;
+      const workedTime = weeklyHours.get(weekNumber) ?? 0;
+      weeklyHours.set(weekNumber, workedTime + activityTimeMs);
+    }
+
+    return weeklyHours;
   }
 }
