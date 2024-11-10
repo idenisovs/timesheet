ede1c96621095ee0238cf85fb0b5ba1436bd7d43
Ilja Denisovs <i.denisovs@gmail.com>
2024-11-10 19:10:59 +0200

    TST-57: Added basic component for Issue Ratio Chart.

---
+++ src/app/pages/analytics-page/analytics-page-charts/issue-ratio-chart/issue-ratio-chart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-issue-ratio-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './issue-ratio-chart.component.html',
+  styleUrl: './issue-ratio-chart.component.scss'
+})
+export class IssueRatioChartComponent {
+
+}
