990f5cf9a66371c365fc67cd1646effa24c1b48a
Ilja Denisovs <i.denisovs@gmail.com>
2024-11-10 19:37:04 +0200

    TST-57: Added Issue Ratio Chart.

--- src/app/pages/analytics-page/analytics-page-charts/analytics-page-charts.component.ts
+++ src/app/pages/analytics-page/analytics-page-charts/analytics-page-charts.component.ts
@@ -4,16 +4,18 @@ import { BaseChartDirective } from 'ng2-charts';
 import { NgIf } from '@angular/common';
 import { ProjectRatioChartComponent } from './project-ratio-chart/project-ratio-chart.component';
 import { WeeksWorkedTimeComponent } from './weeks-worked-time/weeks-worked-time.component';
+import { IssueRatioChartComponent } from './issue-ratio-chart/issue-ratio-chart.component';
 
 @Component({
   selector: 'app-analytics-page-charts',
   standalone: true,
-	imports: [
-		BaseChartDirective,
-		NgIf,
-		ProjectRatioChartComponent,
-		WeeksWorkedTimeComponent,
-	],
+  imports: [
+    BaseChartDirective,
+    NgIf,
+    ProjectRatioChartComponent,
+    WeeksWorkedTimeComponent,
+    IssueRatioChartComponent,
+  ],
   templateUrl: './analytics-page-charts.component.html',
   styleUrl: './analytics-page-charts.component.scss'
 })
