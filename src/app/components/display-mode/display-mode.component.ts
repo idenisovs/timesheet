cec25d3c87136e250ba238031468c59a157baafb
Ilja Denisovs <i.denisovs@gmail.com>
2025-03-21 19:24:42 +0200

    TST-59: Display width in display-mode component.

--- src/app/components/display-mode/display-mode.component.ts
+++ src/app/components/display-mode/display-mode.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { Component, HostListener, inject, OnDestroy, OnInit } from '@angular/core';
 
 import { ScreenService } from '../../services/screen.service';
 import { Subscription } from 'rxjs';
@@ -12,10 +12,16 @@ import { Subscription } from 'rxjs';
 })
 export class DisplayModeComponent implements OnInit, OnDestroy {
   public isMobile = false;
+  public width = window.innerWidth;
 
   private screenService = inject(ScreenService);
 
   private screenServiceSub: Subscription | null = null;
+
+  @HostListener('window:resize')
+  onWindowResize() {
+    this.width = window.innerWidth;
+  }
 
   ngOnInit() {
     this.screenServiceSub = this.screenService.isMobile$.subscribe(value => {
