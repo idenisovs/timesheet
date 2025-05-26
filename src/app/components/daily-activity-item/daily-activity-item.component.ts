32d14100501bf1bf3e2628ca67a75bf0d030200a
Ilja Denisovs <i.denisovs@gmail.com>
2025-05-26 18:44:32 +0300

    TST-60: Activity handlers are moved to service.

--- src/app/components/daily-activity-item/daily-activity-item.component.ts
+++ src/app/components/daily-activity-item/daily-activity-item.component.ts
@@ -1,13 +1,9 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ReactiveFormsModule, UntypedFormBuilder } from '@angular/forms';
 import { NgClass } from '@angular/common';
-import { duration } from 'yet-another-duration';
-import parseDuration from 'parse-duration';
 
 import { ActivityFormGroup } from '../daily-activities-week-day/DailyActivitiesForm';
 import { DailyActivityItemService } from './daily-activity-item.service';
-
-const HOURS_PATTERN_24 = /^([0-2]?[0-3]|[0-1]?[0-9]):[0-5][0-9]$/
 
 @Component({
     selector: 'app-daily-activity-item',
@@ -72,110 +68,15 @@ export class DailyActivityItemComponent implements OnInit {
   ngOnInit(): void {}
 
   handleFromChanges() {
-    const from = this.activity.get('from')?.value;
-    const isFromDefined = HOURS_PATTERN_24.test(from);
-
-    if (!isFromDefined) {
-      return;
-    }
-
-    const till = this.activity.get('till')?.value;
-    const isTillDefined = HOURS_PATTERN_24.test(till);
-
-    if (isTillDefined) {
-      return this.recalculateDuration(from, till);
-    }
-
-    const duration = this.activity.get('duration')?.value;
-
-    if (duration) {
-      this.recalculateTillTime(from, duration);
-    }
+    this.service.handleFromChanges(this.activity);
   }
 
   handleTillChanges() {
-    const till = this.activity.get('till')?.value;
-    const isTillDefined = HOURS_PATTERN_24.test(till);
-
-    if (!isTillDefined) {
-      return;
-    }
-
-    const from = this.activity.get('from')?.value;
-    const isFromDefined = HOURS_PATTERN_24.test(from);
-
-    if (isFromDefined) {
-      return this.recalculateDuration(from, till)
-    }
-
-    const duration = this.activity.get('duration')?.value;
-
-    if (duration) {
-      this.recalculateFromTime(till, duration);
-    }
+    this.service.handleTillChanges(this.activity);
   }
 
   handleDurationChanges() {
-    const duration = this.activity.get('duration')?.value;
-
-    const dT = parseDuration(duration);
-
-    if (!dT) {
-      return;
-    }
-
-    const from = this.activity.get('from')?.value;
-    const isFromDefined = HOURS_PATTERN_24.test(from);
-
-    if (isFromDefined) {
-      return this.recalculateTillTime(from, duration)
-    }
-
-    const till = this.activity.get('till')?.value;
-    const isTillDefined = HOURS_PATTERN_24.test(till);
-
-    if (isTillDefined) {
-      return this.recalculateFromTime(till, duration);
-    }
-  }
-
-  recalculateFromTime(till: string, duration: string) {
-    const d2 = this.service.getDateObj(till);
-
-    const dT = parseDuration(duration);
-
-    const d1 = new Date(d2.getTime() - dT!);
-
-    const fromTime = this.service.getTimeString(d1);
-
-    this.activity.get('from')?.setValue(fromTime);
-  }
-
-  recalculateTillTime(from: string, duration: string) {
-    const d1 = this.service.getDateObj(from);
-
-    const dT = parseDuration(duration) as number;
-
-    const d2 = new Date(d1.getTime() + dT);
-
-    const tillTime = this.service.getTimeString(d2);
-
-    this.activity.get('till')?.setValue(tillTime);
-  }
-
-  recalculateDuration(from: string, till: string) {
-    const d1 = this.service.getDateObj(from);
-    const d2 = this.service.getDateObj(till);
-
-    const dT = d2.getTime() - d1.getTime();
-
-    const durationValue = duration(dT, {
-      units: {
-        min: 'minutes'
-      }
-    }).toString();
-
-    this.activity.get('duration')?.setValue(durationValue);
+    this.service.handleDurationChanges(this.activity);
   }
 
   async copyActivityName() {
@@ -201,17 +102,7 @@ export class DailyActivityItemComponent implements OnInit {
   }
 
   setCurrentTime(field: 'from'|'till') {
-    const formField = this.activity.get(field);
-
-    if (formField) {
-      formField.setValue(this.service.getCurrentTime());
-    }
-
-    if (field === 'from') {
-      this.handleFromChanges();
-    } else {
-      this.handleTillChanges();
-    }
+    this.service.setCurrentTime(this.activity, field);
   }
 
   setTimeFromPreviousActivity() {
