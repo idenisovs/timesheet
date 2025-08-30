17ab31f3c213abf2bb04dda2e3e9cb238835dee1
Ilja Denisovs <i.denisovs@gmail.com>
2025-08-30 17:25:48 +0300

    TST-60: Added Daily Activity Item Mobile component.

---
+++ src/app/components/daily-activity-item-mobile/daily-activity-item-mobile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-daily-activity-item-mobile',
+  imports: [],
+  templateUrl: './daily-activity-item-mobile.component.html',
+  styleUrl: './daily-activity-item-mobile.component.scss'
+})
+export class DailyActivityItemMobileComponent {
+
+}
