a63aeb149896011da34b110e7a5b177cd76f22db
Ilja Denisovs <i.denisovs@gmail.com>
2025-08-30 19:48:17 +0300

    TST-60: UI for Daily Activity Item Mobile.

--- src/app/components/daily-activity-item-mobile/daily-activity-item-mobile.component.ts
+++ src/app/components/daily-activity-item-mobile/daily-activity-item-mobile.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
+import { TimePickerComponent } from './time-picker/time-picker.component';
 
 @Component({
   selector: 'app-daily-activity-item-mobile',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    TimePickerComponent,
+  ],
   templateUrl: './daily-activity-item-mobile.component.html',
   styleUrl: './daily-activity-item-mobile.component.scss'
 })
 export class DailyActivityItemMobileComponent {
+  fb = inject(FormBuilder);
 
+  form = this.fb.group({
+    start: [''],
+    end: ['']
+  });
 }
