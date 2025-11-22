2268e612c964102a47c1abc48923a1180a23166b
Ilja Denisovs <i.denisovs@gmail.com>
2025-11-22 19:53:04 +0200

    TST-60: Added Title component for Activity Item in Mobile mode.

---
+++ src/app/components/daily-activity-item-mobile/activity-item-title/activity-item-title.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-activity-item-title',
+  imports: [],
+  templateUrl: './activity-item-title.component.html',
+  styleUrl: './activity-item-title.component.scss'
+})
+export class ActivityItemTitleComponent {
+
+}
