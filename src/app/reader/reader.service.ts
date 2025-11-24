9748a6bca4519157d798a0919e90ea5a75ce76e4
Ilja Denisovs <i.denisovs@sehner.international>
2025-11-24 12:20:04 +0200

    TST-60: Reuse @for block in Daily Activities Page.

--- src/app/reader/reader.service.ts
+++ src/app/reader/reader.service.ts
@@ -7,54 +7,55 @@ import { ActivityRecord } from './activities/ActivityRecord';
 import { ImportedIssue, ImportedProject } from '../pages/import-page/Imports';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class ReaderService {
-  constructor() { }
+	constructor() {
+	}
 
-  public activities(workbook: XLSX.WorkBook): Activity[] {
-    const metadata = this.readMetadata(workbook);
-    const importer = new ActivityReaderFactory(metadata)
-    const sheet = workbook.Sheets['Activities'];
-    const records = XLSX.utils.sheet_to_json<ActivityRecord>(sheet);
-    return importer.read(records);
-  }
+	public activities(workbook: XLSX.WorkBook): Activity[] {
+		const metadata = this.readMetadata(workbook);
+		const importer = new ActivityReaderFactory(metadata);
+		const sheet = workbook.Sheets['Activities'];
+		const records = XLSX.utils.sheet_to_json<ActivityRecord>(sheet);
+		return importer.read(records);
+	}
 
-  public issues(workbook: XLSX.WorkBook): Issue[] {
-    const issuesSheet = workbook.Sheets['Issues'];
-    const importedIssues: ImportedIssue[] = XLSX.utils.sheet_to_json(issuesSheet);
-    return importedIssues.map(Issue.fromImport);
-  }
+	public issues(workbook: XLSX.WorkBook): Issue[] {
+		const issuesSheet = workbook.Sheets['Issues'];
+		const importedIssues: ImportedIssue[] = XLSX.utils.sheet_to_json(issuesSheet);
+		return importedIssues.map(Issue.fromImport);
+	}
 
-  public projects(workbook: XLSX.WorkBook): Project[] {
-    const projectsSheet = workbook.Sheets['Projects'];
-    const importedProjects: ImportedProject[] = XLSX.utils.sheet_to_json(projectsSheet);
-    return importedProjects.map(Project.fromImport);
-  }
+	public projects(workbook: XLSX.WorkBook): Project[] {
+		const projectsSheet = workbook.Sheets['Projects'];
+		const importedProjects: ImportedProject[] = XLSX.utils.sheet_to_json(projectsSheet);
+		return importedProjects.map(Project.fromImport);
+	}
 
-  private readMetadata(workbook: XLSX.WorkBook): Metadata {
-    const metadataSheet = workbook.Sheets['Metadata'];
+	private readMetadata(workbook: XLSX.WorkBook): Metadata {
+		const metadataSheet = workbook.Sheets['Metadata'];
 
-    if (!metadataSheet) {
-      return new Metadata();
-    }
+		if (!metadataSheet) {
+			return new Metadata();
+		}
 
-    const metadataJson: MetadataRecord[] = XLSX.utils.sheet_to_json(metadataSheet);
+		const metadataJson: MetadataRecord[] = XLSX.utils.sheet_to_json(metadataSheet);
 
-    return {
-      activities: this.readMetadataField(metadataJson, MetadataFieldType.activities)
-    };
-  }
+		return {
+			activities: this.readMetadataField(metadataJson, MetadataFieldType.activities)
+		};
+	}
 
-  private readMetadataField(json: MetadataRecord[], type: MetadataFieldType): MetadataField {
-    const metadataFieldData = json.find(record => record.type === type);
+	private readMetadataField(json: MetadataRecord[], type: MetadataFieldType): MetadataField {
+		const metadataFieldData = json.find(record => record.type === type);
 
-    if (!metadataFieldData) {
-      return new MetadataField();
-    }
+		if (!metadataFieldData) {
+			return new MetadataField();
+		}
 
-    return {
-      version: metadataFieldData.version
-    };
-  }
+		return {
+			version: metadataFieldData.version
+		};
+	}
 }
