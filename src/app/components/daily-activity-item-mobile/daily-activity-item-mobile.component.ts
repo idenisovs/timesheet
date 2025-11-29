0af9d5257ad136d1f0bc713f8d5250577dc71f69
Ilja Denisovs <i.denisovs@gmail.com>
2025-11-29 15:41:25 +0200

    TST-60: Pass Activity Name into modal Edit Form in mobile mode.

--- src/app/components/daily-activity-item-mobile/daily-activity-item-mobile.component.ts
+++ src/app/components/daily-activity-item-mobile/daily-activity-item-mobile.component.ts
@@ -1,11 +1,11 @@
 import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import {
-  NgbDropdown,
-  NgbDropdownMenu,
-  NgbDropdownToggle,
-  NgbModal,
-  NgbModalOptions,
+	NgbDropdown,
+	NgbDropdownMenu,
+	NgbDropdownToggle,
+	NgbModal,
+	NgbModalOptions, NgbModalRef,
 } from '@ng-bootstrap/ng-bootstrap';
 
 import { TimePickerComponent } from './time-picker/time-picker.component';
@@ -15,76 +15,79 @@ import { MenuItemComponent } from './menu-item/menu-item.component';
 import { ActivityItemTitleComponent } from './activity-item-title/activity-item-title.component';
 
 type ActivityValue = {
-  id: string | null;
-  name: string | null;
-  from: string | null;
-  till: string | null;
-  duration: string | null;
+	id: string | null;
+	name: string | null;
+	from: string | null;
+	till: string | null;
+	duration: string | null;
 };
 
 @Component({
-  selector: 'app-daily-activity-item-mobile',
-  imports: [
-    ReactiveFormsModule,
-    TimePickerComponent,
-    NgbDropdown,
-    NgbDropdownToggle,
-    NgbDropdownMenu,
-    MenuItemComponent,
-    ActivityItemTitleComponent,
-  ],
-  templateUrl: './daily-activity-item-mobile.component.html',
-  styleUrl: './daily-activity-item-mobile.component.scss'
+	selector: 'app-daily-activity-item-mobile',
+	imports: [
+		ReactiveFormsModule,
+		TimePickerComponent,
+		NgbDropdown,
+		NgbDropdownToggle,
+		NgbDropdownMenu,
+		MenuItemComponent,
+		ActivityItemTitleComponent,
+	],
+	templateUrl: './daily-activity-item-mobile.component.html',
+	styleUrl: './daily-activity-item-mobile.component.scss',
 })
 export class DailyActivityItemMobileComponent {
-  fb = inject(FormBuilder);
-  service = inject(DailyActivityItemService);
-  modalService = inject(NgbModal);
+	fb = inject(FormBuilder);
+	service = inject(DailyActivityItemService);
+	modalService = inject(NgbModal);
 
-  @Input()
-  activityForm = this.fb.group<ActivityValue>({
-    id: '',
-    name: '',
-    from: '',
-    till: '',
-    duration: ''
-  });
+	@Input()
+	activityForm = this.fb.group<ActivityValue>({
+		id: '',
+		name: '',
+		from: '',
+		till: '',
+		duration: '',
+	});
 
-  @Output()
-  add = new EventEmitter<string>();
+	@Output()
+	add = new EventEmitter<string>();
 
-  @Output()
-  copy = new EventEmitter<string>();
+	@Output()
+	copy = new EventEmitter<string>();
 
-  @Output()
-  remove = new EventEmitter<string>();
+	@Output()
+	remove = new EventEmitter<string>();
 
-  get ActivityId(): string {
-    return this.activityForm.get('id')?.value as string;
-  }
+	get ActivityId(): string {
+		return this.activityForm.get('id')?.value as string;
+	}
 
-  get ActivityName(): string {
-    return this.activityForm.get('name')?.value ?? '';
-  }
+	get ActivityName(): string {
+		return this.activityForm.get('name')?.value ?? '';
+	}
 
-  handleFromChanges() {
-    this.service.handleFromChanges(this.activityForm);
-  }
+	handleFromChanges() {
+		this.service.handleFromChanges(this.activityForm);
+	}
 
-  handleTillChanges() {
-    this.service.handleTillChanges(this.activityForm);
-  }
+	handleTillChanges() {
+		this.service.handleTillChanges(this.activityForm);
+	}
 
-  async openEditModal() {
-    const options: NgbModalOptions = {
-      centered: true,
-    };
+	async openEditModal() {
+		const options: NgbModalOptions = {
+			centered: false,
+		};
 
-    try {
-      const name = await this.modalService.open(ActivityItemEditModalComponent, options).result as string;
-      this.activityForm.get('name')?.setValue(name);
-    } catch (e) {
-      console.log(e);
-    }
-  }
+		try {
+			const activityEditModal: NgbModalRef = this.modalService.open(ActivityItemEditModalComponent, options);
+			const instance = activityEditModal.componentInstance as ActivityItemEditModalComponent;
+			instance.name = this.ActivityName;
+			const nameUpdate = await activityEditModal.result as string;
+			this.activityForm.get('name')?.setValue(nameUpdate);
+		} catch (e) {
+			console.log(e);
+		}
+	}
 }
