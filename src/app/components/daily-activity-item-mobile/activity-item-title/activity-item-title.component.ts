09a3a7bbdba71483a1c77008167cad91f06ffb7b
Ilja Denisovs <i.denisovs@gmail.com>
2025-11-29 16:22:43 +0200

    TST-60: Trim the ticket from activity name.

--- src/app/components/daily-activity-item-mobile/activity-item-title/activity-item-title.component.ts
+++ src/app/components/daily-activity-item-mobile/activity-item-title/activity-item-title.component.ts
@@ -1,12 +1,30 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-activity-item-title',
-  imports: [],
-  templateUrl: './activity-item-title.component.html',
-  styleUrl: './activity-item-title.component.scss'
+	selector: 'app-activity-item-title',
+	imports: [],
+	templateUrl: './activity-item-title.component.html',
+	styleUrl: './activity-item-title.component.scss',
 })
 export class ActivityItemTitleComponent {
-  @Input()
-  activityName!: string;
+	private static readonly DEFAULT_TICKET = 'Activity';
+
+	@Input()
+	activityName: string = '';
+
+	get ActivityTicket(): string {
+		const ticketSplitterIdx = this.activityName.indexOf(':');
+
+		if (ticketSplitterIdx === -1) {
+			return ActivityItemTitleComponent.DEFAULT_TICKET;
+		}
+
+		const ticket: string = this.activityName.slice(0, ticketSplitterIdx).trim();
+
+		if (!ticket) {
+			return ActivityItemTitleComponent.DEFAULT_TICKET;
+		}
+
+		return ticket;
+	}
 }
