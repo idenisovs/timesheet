f129243e4f26d3c624b07fc252fc0fbf33645e21
Ilja Denisovs <i.denisovs@sehner.international>
2026-02-03 20:48:45 +0200

    Use Inject instead of constructor.

--- src/app/pages/daily-activities-page/daily-activities-page.component.ts
+++ src/app/pages/daily-activities-page/daily-activities-page.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
 
@@ -12,72 +12,69 @@ import { delay } from '../../utils';
 import { DailyActivitiesWeekComponent } from '../../components/daily-activities-week/daily-activities-week.component';
 
 @Component({
-    selector: 'app-daily-activities-page',
-    templateUrl: './daily-activities-page.component.html',
-    styleUrls: ['./daily-activities-page.component.scss'],
-    standalone: true,
+	selector: 'app-daily-activities-page',
+	templateUrl: './daily-activities-page.component.html',
+	styleUrls: ['./daily-activities-page.component.scss'],
+	standalone: true,
 	imports: [InfiniteScrollDirective, DailyActivitiesWeekComponent],
 })
 export class DailyActivitiesPageComponent implements OnInit, AfterViewInit, OnDestroy {
-  weeks: Week[] = [];
-  offset = 0;
+	private router = inject(Router);
+	private weekRepo = inject(WeeksRepositoryService);
+	private actionsService = inject(ActionsService);
+	private exportWorkflow = inject(ExportWorkflowService);
+	private prepareForTodayWorkflow = inject(PrepareForTodayWorkflowService);
+	private actionSubs = this.actionsService.on.subscribe(this.handlePageActions.bind(this));
 
-  @ViewChild('weeksList') weekListRef!: ElementRef;
+	@ViewChild('weeksList') weekListRef!: ElementRef;
 
-  private actionSubs = this.actionsService.on.subscribe(this.handlePageActions.bind(this));
+	weeks: Week[] = [];
+	offset = 0;
 
-  constructor(
-    private router: Router,
-    private weekRepo: WeeksRepositoryService,
-    private actionsService: ActionsService,
-    private exportWorkflow: ExportWorkflowService,
-    private prepareForTodayWorkflow: PrepareForTodayWorkflowService
-  ) { }
+	async ngOnInit() {
+		await this.prepareForTodayWorkflow.run();
+		await this.loadNextWeek();
+	}
 
-  async ngOnInit() {
-    await this.prepareForTodayWorkflow.run();
-    await this.loadNextWeek();
-  }
+	async ngAfterViewInit() {
+		await this.preloadWeeks();
+	}
 
-  async ngAfterViewInit() {
-    await this.preloadWeeks();
-  }
+	ngOnDestroy() {
+		this.actionSubs.unsubscribe();
+	}
 
-  ngOnDestroy() {
-    this.actionSubs.unsubscribe();
-  }
+	async preloadWeeks() {
+		await delay(150);
 
-  async preloadWeeks() {
-    await delay(150);
+		const weekListHeight = (this.weekListRef.nativeElement as HTMLElement).offsetHeight;
+		const windowHeight = window.innerHeight;
+		const numberOfWeeks = await this.weekRepo.getCount();
 
-    const weekListHeight = (this.weekListRef.nativeElement as HTMLElement).offsetHeight;
-    const windowHeight = window.innerHeight;
-    const numberOfWeeks = await this.weekRepo.getCount();
+		if (weekListHeight <= windowHeight && this.weeks.length < numberOfWeeks) {
+			await this.loadNextWeek();
+			void this.preloadWeeks();
+		}
+	}
 
-    if (weekListHeight <= windowHeight && this.weeks.length < numberOfWeeks) {
-      await this.loadNextWeek();
-      void this.preloadWeeks();
-    }
-  }
+	public async loadNextWeek() {
+		const week = await this.weekRepo.getByOffset(this.offset);
 
-  public async loadNextWeek() {
-    const week = await this.weekRepo.getByOffset(this.offset);
+		this.offset += 1;
 
-    this.offset += 1;
+		if (week) {
+			this.weeks.push(week);
+		}
+	}
 
-    if (week) {
-      this.weeks.push(week);
-    }
-  }
-
-  private async handlePageActions(action: Actions) {
-    switch (action) {
-      case Actions.Export:
-        await this.exportWorkflow.export();
-        break;
-      case Actions.Import:
-        await this.router.navigate(['import']);
-        break;
-    }
-  }
+	private async handlePageActions(action: Actions) {
+		switch (action) {
+			case Actions.Export:
+				await this.exportWorkflow.export();
+				break;
+			case Actions.Import:
+				await this.router.navigate(['import']);
+				break;
+		}
+	}
 }
