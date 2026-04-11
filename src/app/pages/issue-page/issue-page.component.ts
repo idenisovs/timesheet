dba21025f135642b11462b2946df99606292cf8b
Ilja Denisovs <i.denisovs@gmail.com>
2026-04-11 17:14:07 +0300

    Update formatting and apply the inject approach in issue-page.component.ts

--- src/app/pages/issue-page/issue-page.component.ts
+++ src/app/pages/issue-page/issue-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { ReactiveFormsModule } from '@angular/forms';
@@ -10,42 +10,40 @@ import { ActivitiesRepositoryService } from '../../repository/activities-reposit
 import { IssueRepositoryService } from '../../repository/issue-repository.service';
 
 @Component({
-    selector: 'app-issue-page',
-    imports: [
-    ReactiveFormsModule,
-    ActivitiesListComponent,
-    IssueCardComponent
-],
-    templateUrl: './issue-page.component.html',
-    styleUrl: './issue-page.component.scss'
+	selector: 'app-issue-page',
+	imports: [
+		ReactiveFormsModule,
+		ActivitiesListComponent,
+		IssueCardComponent,
+	],
+	templateUrl: './issue-page.component.html',
+	styleUrl: './issue-page.component.scss',
 })
 export class IssuePageComponent implements OnInit {
-  issue?: Issue;
-  activities: Activity[] = [];
-  issueKey!: string;
-  isNotFound: boolean = false;
+	private route = inject(ActivatedRoute);
+	private activityRepository = inject(ActivitiesRepositoryService);
+	private issueRepository = inject(IssueRepositoryService);
 
-  constructor(
-    private route: ActivatedRoute,
-    private activityRepository: ActivitiesRepositoryService,
-    private issueRepository: IssueRepositoryService
-  ) {}
+	issue?: Issue;
+	activities: Activity[] = [];
+	issueKey!: string;
+	isNotFound: boolean = false;
 
-  ngOnInit() {
-    this.route.params.subscribe(async ({ issueKey }) => {
-      this.issueKey = issueKey;
-      await this.loadIssue(issueKey);
-    });
-  }
+	ngOnInit() {
+		this.route.params.subscribe(async ({issueKey}) => {
+			this.issueKey = issueKey;
+			await this.loadIssue(issueKey);
+		});
+	}
 
-  async loadIssue(issueKey: string) {
-    const existingIssue = await this.issueRepository.getByKey(issueKey);
+	async loadIssue(issueKey: string) {
+		const existingIssue = await this.issueRepository.getByKey(issueKey);
 
-    if (existingIssue) {
-      this.issue = existingIssue;
-      this.activities = await this.activityRepository.getByIssueKey(this.issueKey);
-    } else {
-      this.isNotFound = true;
-    }
-  }
+		if (existingIssue) {
+			this.issue = existingIssue;
+			this.activities = await this.activityRepository.getByIssueKey(this.issueKey);
+		} else {
+			this.isNotFound = true;
+		}
+	}
 }
