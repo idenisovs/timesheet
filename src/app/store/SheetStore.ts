6151a08ebb085dfa5fea47a28eb8f6afa41237da
Ilja Denisovs <i.denisovs@gmail.com>
2026-04-17 15:16:16 +0300

    TST-66: Remove Sheet, Week and Day tables.

--- src/app/store/SheetStore.ts
+++ src/app/store/SheetStore.ts
@@ -10,56 +10,54 @@ import migrateV8 from './migrate-v8';
 import migrateV9 from './migrate-v9';
 
 export default class SheetStore extends Dexie {
-	sheet: Dexie.Table<SheetRecord, string>;
 	issues: Dexie.Table<IssueRecord, string>;
 	projects: Dexie.Table<ProjectRecord, string>;
-
-	weeks: Dexie.Table<WeekRecord, string>;
-	days: Dexie.Table<DayRecord, string>;
 	activities: Dexie.Table<ActivityRecord, string>;
 
 	constructor() {
 		super('timesheet');
 
 		this.version(1).stores({
-			sheet: '++id,date,activities'
+			sheet: '++id,date,activities',
 		});
 
 		this.version(2).stores({
-			issues: '++id,&key,name,createdAt'
+			issues: '++id,&key,name,createdAt',
 		}).upgrade((tx: Transaction) => migrateV2(this, tx));
 
 		this.version(3).stores({
-			issues: '++id,&key,name,activities,createdAt'
+			issues: '++id,&key,name,activities,createdAt',
 		}).upgrade((tx: Transaction) => migrateV3(this, tx));
 
 		this.version(4).stores({
-			projects: 'id,&name,description,*keys,createdAt'
+			projects: 'id,&name,description,*keys,createdAt',
 		});
 
 		this.version(5).stores({
 			weeks: 'id,from,till',
 			days: 'id,&date,weekId',
 			activities: 'id,name,date,weekId,dayId',
-			issues: '++id,&key,name,*activities,createdAt'
+			issues: '++id,&key,name,*activities,createdAt',
 		}).upgrade((tx: Transaction) => migrateV5(this, tx));
 
 		this.version(6).upgrade((tx: Transaction) => tx.table('sheet').clear());
 		this.version(7).upgrade((tx: Transaction) => migrateV7(this, tx));
 		this.version(8).stores({
 			activities: '&id,name,date,weekId,dayId,issueId',
-			issues: '++id,&key,name,activities,createdAt'
+			issues: '++id,&key,name,activities,createdAt',
 		}).upgrade((tx: Transaction) => migrateV8(this, tx));
 		this.version(9).stores({
 			weeks: '&id,&from,&till',
 		}).upgrade((tx: Transaction) => migrateV9(this, tx));
 
-		this.sheet = this.table('sheet');
-		this.issues = this.table('issues');
-		this.projects = this.table('projects');
+		this.version(10).stores({
+			sheet: null,
+			weeks: null,
+			days: null,
+		});
 
-		this.weeks = this.table('weeks');
-		this.days = this.table('days');
+		this.projects = this.table('projects');
+		this.issues = this.table('issues');
 		this.activities = this.table('activities');
 	}
 }
