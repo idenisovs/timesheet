6e59e8322e9a9a981756ab729eb581cffd2a6baf
Ilja Denisovs <i.denisovs@gmail.com>
2026-04-25 12:17:40 +0300

    I have now the separate component for app version indicator.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,19 @@
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
 import { Subscription } from 'rxjs';
-import { DateTime } from 'luxon';
 
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { AppVersionComponent } from './components/app-version/app-version.component';
 import { ColorModeService } from './services/color-mode.service';
 import { ActionsService } from './services/actions.service';
 import { Actions } from './services/Actions';
-import { version, build } from '../environments/version';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 	standalone: true,
-	imports: [RouterOutlet, NavbarComponent, NgbTooltip],
+	imports: [RouterOutlet, NavbarComponent, AppVersionComponent],
 })
 export class AppComponent implements OnInit, OnDestroy {
 	private actions = inject(ActionsService);
@@ -23,8 +21,6 @@ export class AppComponent implements OnInit, OnDestroy {
 
 	title = 'timesheet';
 	actionsSubs!: Subscription;
-	version = version;
-	buildAgo = DateTime.fromISO(build).toRelative();
 
 	ngOnInit() {
 		this.actionsSubs = this.actions.on.subscribe(this.handlePageActions.bind(this));
---
+++ src/app/components/app-version/app-version.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
+import { DateTime } from 'luxon';
+
+import { version, build } from '../../../environments/version';
+
+@Component({
+	selector: 'app-version',
+	imports: [NgbTooltip],
+	templateUrl: './app-version.component.html',
+	styleUrl: './app-version.component.scss',
+})
+export class AppVersionComponent {
+	version = version;
+	buildAgo = DateTime.fromISO(build).toRelative();
+}
