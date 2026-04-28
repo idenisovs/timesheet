6495da0297983c69e824a887e0117f3597283e38
Ilja Denisovs <i.denisovs@gmail.com>
2026-04-28 10:06:54 +0300

    TST-69: Remove outdated DB versions.

--- src/app/store/SheetStore.ts
+++ src/app/store/SheetStore.ts
@@ -2,8 +2,6 @@ import Dexie, { Transaction } from 'dexie';
 
 import { ProjectRecord, IssueRecord, ActivityRecord } from './records';
 
-import migrateV2 from './migrate-v2';
-import migrateV3 from './migrate-v3';
 import migrateV5 from './migrate-v5';
 import migrateV7 from './migrate-v7';
 import migrateV8 from './migrate-v8';
@@ -19,18 +17,6 @@ export default class SheetStore extends Dexie {
 
 	constructor() {
 		super('timesheet');
-
-		this.version(1).stores({
-			sheet: '++id,date,activities',
-		});
-
-		this.version(2).stores({
-			issues: '++id,&key,name,createdAt',
-		}).upgrade((tx: Transaction) => migrateV2(this, tx));
-
-		this.version(3).stores({
-			issues: '++id,&key,name,activities,createdAt',
-		}).upgrade((tx: Transaction) => migrateV3(this, tx));
 
 		this.version(4).stores({
 			projects: 'id,&name,description,*keys,createdAt',
