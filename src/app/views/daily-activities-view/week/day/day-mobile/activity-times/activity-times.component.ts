ec24f2fcb9dc265072ed57e692e78ca2fdd81cc8
Ilja Denisovs <i.denisovs@gmail.com>
2026-05-20 09:56:24 +0300

    TST-72: Added colorful styling for time indicators.

--- src/app/views/daily-activities-view/week/day/day-mobile/activity-times/activity-times.component.ts
+++ src/app/views/daily-activities-view/week/day/day-mobile/activity-times/activity-times.component.ts
@@ -1,12 +1,19 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input, InputSignal, Signal } from '@angular/core';
+
+import { Activity } from '../../../../../../entities';
+import { DarkerColorDirective } from '../../../../../../directives/darker-color.directive';
+import { SubtleColorDirective } from '../../../../../../directives/subtle-color.directive';
 
 @Component({
   selector: 'app-activity-times',
-  imports: [],
+  imports: [DarkerColorDirective, SubtleColorDirective],
   templateUrl: './activity-times.component.html',
   styleUrl: './activity-times.component.scss',
 })
 export class ActivityTimesComponent {
-	from = input<string>();
-	till = input<string>();
+	activity: InputSignal<Activity> = input.required<Activity>();
+
+	from: Signal<string> = computed<string>(() => this.activity().from);
+	till: Signal<string> = computed<string>(() => this.activity().till);
+	color: Signal<string | undefined> = computed<string | undefined>(() => this.activity().color);
 }
