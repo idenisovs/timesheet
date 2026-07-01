9e63ffee70c1cfa86a2f7c49cd1fb9bd0dd7cc01
Ilja Denisovs <i.denisovs@gmail.com>
2026-07-01 18:10:41 +0300

    TST-72: Layout for Activity Edit Modal inputs.

--- src/app/views/daily-activities-view/week/day/day-mobile/activity-edit-modal/activity-edit-modal.component.ts
+++ src/app/views/daily-activities-view/week/day/day-mobile/activity-edit-modal/activity-edit-modal.component.ts
@@ -1,8 +1,6 @@
-import { Component, ElementRef, inject, Input, signal, viewChild } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { DateTime } from 'luxon';
-import parseDuration from 'parse-duration';
 
 import { Activity } from '../../../../../../entities';
 import { ActivityFormGroup } from '../../DailyActivitiesForm';
@@ -43,32 +41,6 @@ export class ActivityEditModalComponent {
 		}
 	}
 
-	get RemainingTime(): number {
-		const previous = DateTime.fromFormat(this.PreviousTime, 'H:mm');
-		const next = DateTime.fromFormat(this.NextTime, 'H:mm');
-		return next.diff(previous, 'minutes').minutes;
-	}
-
-	get ActivityDuration(): number {
-		return parseDuration(this.activity.duration, 'm') ?? 0;
-	}
-
-	get Container(): HTMLElement {
-		return this.cardContainer().nativeElement;
-	}
-
-	get Card(): HTMLElement {
-		return this.card().nativeElement;
-	}
-
-	get ContainerBottom(): number {
-		return Math.round(this.Container.getBoundingClientRect().bottom);
-	}
-
-	get CardBottom(): number {
-		return Math.round(this.Card.getBoundingClientRect().bottom);
-	}
-
 	@Input()
 	public activity!: Activity;
 
@@ -81,50 +53,7 @@ export class ActivityEditModalComponent {
 	@Input()
 	public nextActivity?: Activity;
 
-	isPicked = signal<boolean>(false);
-	startY = 0;
-	startHeight = 0;
-	maxBottom = 0;
-
-	private readonly card = viewChild.required<ElementRef<HTMLElement>>('card');
-	private readonly cardContainer = viewChild.required<ElementRef<HTMLElement>>('cardContainer');
-
 	protected setCurrentTime(field: 'from' | 'till') {
 		this.service.setCurrentTime(this.activityFormItem, field);
-	}
-
-	protected onPointerDown(event: PointerEvent) {
-		this.isPicked.set(true);
-		this.startY = event.clientY;
-		this.startHeight = this.Card.offsetHeight;
-		this.maxBottom = this.ContainerInnerBottom;
-	}
-
-	protected onPointerMove(event: PointerEvent) {
-		const diff = event.clientY - this.startY;
-		const updateHeight = this.startHeight + diff;
-		this.Card.style.height = `${updateHeight}px`;
-
-		const overshoot = this.CardOuterBottom - this.maxBottom;
-
-		if (overshoot > 0) {
-			this.Card.style.height = `${updateHeight - overshoot}px`;
-		}
-	}
-
-	private get CardOuterBottom(): number {
-		const cardStyle = getComputedStyle(this.Card);
-		const marginBottom = parseFloat(cardStyle.marginBottom);
-		return this.Card.getBoundingClientRect().bottom + marginBottom;
-	}
-
-	private get ContainerInnerBottom(): number {
-		const containerStyle = getComputedStyle(this.Container);
-		const paddingBottom = parseFloat(containerStyle.paddingBottom);
-		return this.Container.getBoundingClientRect().bottom - paddingBottom;
-	}
-
-	protected onPointerUp(event: PointerEvent) {
-		this.isPicked.set(false);
 	}
 }
